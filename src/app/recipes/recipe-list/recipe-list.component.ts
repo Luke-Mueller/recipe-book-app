93786aea9c34f5d78da198cf1240a66f30e73beb
Luke Mueller <muelll79@hotmail.com>
2020-02-07 13:57:24 -0700

    adds recipe modal and renders recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Test Desc', 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.justataste.com%2Fwp-content%2Fuploads%2F2016%2F02%2Fquick-crispy-vegetable-fritters-recipe-1.jpg&f=1&nofb=1'),
+    new Recipe('Test 2', 'Test Desc2', 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.justataste.com%2Fwp-content%2Fuploads%2F2016%2F02%2Fquick-crispy-vegetable-fritters-recipe-1.jpg&f=1&nofb=1')
+  ];
 
   constructor() { }
 
